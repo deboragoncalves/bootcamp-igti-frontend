9fb1b1da712a4a3d8e6f8dc0835a108632130ca7
Débora Gonçalves <deboracristinapereira123@gmail.com>
2021-08-24 15:27:09 -0300

    Layout - Acai

--- modulo2/desafios/desafio01/acai/src/app/app.module.ts
+++ modulo2/desafios/desafio01/acai/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SelectComponentComponent } from 'select-component/select-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ modulo2/desafios/desafio01/acai/src/select-component/select-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'select-component',
+  templateUrl: './select-component.component.html',
+  styleUrls: ['./select-component.component.css']
+})
+export class SelectComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
