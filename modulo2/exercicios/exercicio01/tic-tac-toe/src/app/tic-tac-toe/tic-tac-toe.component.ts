182683c57c73b455d0b978bcc3e967bc7c99ddec
Débora Gonçalves <deboracristinapereira123@gmail.com>
2021-08-29 23:49:20 -0300

    Reiniciar partida, trocar jogador, verificar vencedor - Tic tac toe

--- modulo2/exercicios/exercicio01/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ modulo2/exercicios/exercicio01/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -7,16 +7,83 @@ import { Component } from '@angular/core';
 })
 export class TicTacToeComponent {
 
-  currentPlayer: string = "X";
-  play: string[][] = [
-    ["X", "X", "X"],
-    ["", "", ""],
-    ["", "", ""]
-  ];
-  endPlay: boolean;
+  winner: string;
+  currentPlayer: string;
+  play: string[][];
+
+  ngOnInit() {
+    this.play = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ];
+
+    this.winner = "";
+    this.currentPlayer = "X";
+  }
 
   playTicTac(row: number, column: number): void {
-    console.log(row);
-    console.log(column);
+    if (this.play[row][column] == "" && this.winner == "") {
+      this.play[row][column] = this.currentPlayer;
+    }
+
+    this.changeCurrentPlayer();
+    this.checkWinner();
+
+    if (this.checkWinner() == true) {
+      this.winner = this.currentPlayer;
+    }
+  }
+
+  changeCurrentPlayer(): void {
+    if (this.currentPlayer === "X") {
+      this.currentPlayer = "O";
+    } else if (this.currentPlayer === "O") {
+      this.currentPlayer = "X";
+    }
+  }
+
+  restartPlay(): void {
+    this.play = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ];
+
+    this.winner = "";
+    this.currentPlayer = "X";
+  }
+
+  checkWinner(): boolean {
+
+    for (let i = 0; i < this.play.length; i++) {
+      // Linhas
+
+      if (this.play[i][0] == this.currentPlayer && this.play[i][1] == this.currentPlayer && this.play[i][2] == this.currentPlayer) {
+        return true;
+      }
+    }
+
+    for (let i = 0; i < this.play.length; i++) {
+      // Colunas
+
+      if (this.play[0][i] === this.currentPlayer && this.play[1][i] === this.currentPlayer && this.play[2][i] === this.currentPlayer) {
+        return true;
+      }
+    }
+
+    // Diagonais
+
+    if (this.play[0][0] != "" && (this.play[0][0] === this.play[1][1]) && (this.play[0][0] === this.play[2][2])) {
+      return true;
+    }
+
+
+    if (this.play[2][0] != "" && (this.play[2][0] === this.play[1][1]) && (this.play[2][0] === this.play[0][2])) {
+      return true;
+    }
+
+    // TODO: correção, winner após próxima jogada
+
   }
 }
