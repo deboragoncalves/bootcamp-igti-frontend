81dbfd72ee0007efa9d553fabf77850449f1be8d
debora.goncalves <deboracristinapereira123@gmail.com>
2021-09-05 04:08:39 -0300

    Inicio projeto - Lista Contatos CRUD

--- modulo2/exercicios/exercicio03/contacts-list-crud/src/app/app.module.ts
+++ modulo2/exercicios/exercicio03/contacts-list-crud/src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,8 +12,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule
