50810a6332e7e8bbe3d7ba3430cd4a95686e41c8
debora.goncalves <deboracristinapereira123@gmail.com>
2021-09-07 01:07:24 -0300

    Total de pedidos e valor total - Ng Food

--- angular/desafios/desafio02/ngFood/src/app/menu/menu.component.ts
+++ angular/desafios/desafio02/ngFood/src/app/menu/menu.component.ts
@@ -14,6 +14,13 @@ export class MenuComponent implements OnInit {
 
   options: Array<any> = new Array();
 
+  juiceRequests: number = 0;
+  acaiRequests: number = 0;
+  sandwichRequests: number = 0;
+
+  totalRequests: number = 0;
+  totalValue: number = 0;
+
   constructor(private foodRequestsService: FoodRequestsService) { }
 
   ngOnInit(): void {
@@ -28,6 +35,94 @@ export class MenuComponent implements OnInit {
       this.sandwhices = this.options[1];
       this.acai = this.options[2];
     })
+  }
+
+  juiceRequest(juice: any) {
+
+    let juiceRequestObject = {};
+
+    if (juice.name == "Suco de laranja") {
+      this.juiceRequests++;
+
+      juiceRequestObject = {
+        requests: this.juiceRequests,
+        juice: juice.name,
+        total: juice.price * this.juiceRequests
+      }
+
+      this.totalValue += JSON.parse(JSON.stringify(juiceRequestObject)).total;
+    } else if (juice.name == "Suco de uva") {
+      this.juiceRequests++;
+
+      juiceRequestObject = {
+        requests: this.juiceRequests,
+        juice: juice.name,
+        total: juice.price * this.juiceRequests
+      }
+
+      this.totalValue += JSON.parse(JSON.stringify(juiceRequestObject)).total;
+    }
+
+    this.totalRequests++;
+  }
+
+  acaiRequest(acai: any) {
+
+    let acaiRequestObject = {};
+
+    if (acai.name.includes("Leite ninho")) {
+      this.acaiRequests++;
+
+      acaiRequestObject = {
+        requests: this.acaiRequests,
+        acai: acai.name,
+        total: acai.price * this.acaiRequests
+      }  
+
+      this.totalValue += JSON.parse(JSON.stringify(acaiRequestObject)).total;
+    } else if (acai.name.includes("puro")) {
+      this.acaiRequests++;
+
+      acaiRequestObject = {
+        requests: this.acaiRequests,
+        acai: acai.name,
+        total: acai.price * this.acaiRequests
+      }
+
+      this.totalValue += JSON.parse(JSON.stringify(acaiRequestObject)).total;
+    }
+
+    this.totalRequests++;
+  }
+
+  sandwichRequest(sandwich: any) {
+
+    let sandwichRequestObject = {};
+
+    if (sandwich.name.includes("Egg")) {
+      this.sandwichRequests++;
+
+      sandwichRequestObject = {
+        requests: this.sandwichRequests,
+        sandwich: sandwich.name,
+        total: sandwich.price * this.sandwichRequests
+      }
+      
+      this.totalValue += JSON.parse(JSON.stringify(sandwichRequestObject)).total;
+
+    } else if (sandwich.name.includes("Burguer")) {
+      this.sandwichRequests++;
+
+      sandwichRequestObject = {
+        requests: this.juiceRequests,
+        sandwich: sandwich.name,
+        total: sandwich.price * this.sandwichRequests
+      }
+
+      this.totalValue += JSON.parse(JSON.stringify(sandwichRequestObject)).total;
+    }
+
+    this.totalRequests++;
   }
 
 }
