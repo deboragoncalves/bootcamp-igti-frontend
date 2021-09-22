810e26497877a4b1b417bef07ee85a22234ee523
debora.goncalves <deboracristinapereira123@gmail.com>
2021-09-21 21:22:08 -0300

    Layout - IMC App

--- vue/exercicios/imc-app/src/App.vue
+++ vue/exercicios/imc-app/src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 id="title">Cálculo do IMC</h1>
+  <form class="form">
+    <div class="input">
+      <label class="form-label" id="weight">Peso:</label>
+      <input class="form-control" type="text" id="weight" tabindex="0" />
+    </div>
+    <div class="input">
+      <label class="form-label" id="height">Altura:</label>
+      <input class="form-control" id="height" type="text" tabindex="0" />
+    </div>
+    <button class="btn btn-outline-secondary" type="button">Calcular</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "IMC App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
+
+#title {
+  font-size: 25px;
+  margin: 20px;
+  font-weight: bold;
+}
+
+.form {
+  width: 30%;
+  margin: 10px;
+}
+
+.input {
+  display: flex;
+}
+
+.input label {
+  margin: 10px;
+}
+
+.input input {
+  margin: 10px;
+}
+
+button {
+  margin: 10px;
+  display: flex;
+}
+
 </style>
