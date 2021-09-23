0fed27a253b0895635a3c3159871c90d33e9e672
debora.goncalves <deboracristinapereira123@gmail.com>
2021-09-22 21:21:34 -0300

    Layout - Real/Dollar

--- vue/desafios/real-dollar/src/App.vue
+++ vue/desafios/real-dollar/src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <p id="title">Conversão Real para Dólar</p>
+    <form class="form">
+      <div class="input">
+        <label for="real">Reais (R$):</label>
+        <input id="real" class="form-control" type="text">
+      </div>
+      <div class="input">
+        <label for="dolar">Cotação do dólar ($):</label>
+        <input id="dolar" class="form-control" type="text">
+      </div>
+      <div class="buttons">
+        <button class="btn btn-outline-success" type="button">Calcular</button>
+        <button class="btn btn-outline-success" type="button">Limpar</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Real/Dollar"
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
+
+#title {
+  font-size: 25px;
+  font-weight: bold;
+  margin: 15px;
+}
+
+.form {
+  width: 35%;
+  margin: 20px 0;
+}
+
+.input, .buttons {
+  display: flex;
+}
+
+.input {
+  margin: 15px;
+}
+
+.input label {
+  width: 300px;
+}
+
+.buttons button {
+  margin: 10px;
+}
+
 </style>
