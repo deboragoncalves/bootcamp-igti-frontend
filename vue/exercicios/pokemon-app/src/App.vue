5fd679b60211766b3e90d69523b3ab832679bed5
debora.goncalves <deboracristinapereira123@gmail.com>
2021-09-26 15:57:51 -0300

    Inicio projeto - Pokemon

---
+++ vue/exercicios/pokemon-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">Hello</div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
