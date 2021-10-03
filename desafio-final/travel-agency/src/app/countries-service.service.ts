4126e7309ba624dd2b9639284861585fb30c9dfc
debora.goncalves <deboracristinapereira123@gmail.com>
2021-10-03 01:16:51 -0300

    Criar serviço e consumir dados dos paises - Agência de Viagem

---
+++ desafio-final/travel-agency/src/app/countries-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  baseUrl: string = "http://localhost:3000/"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCountriesInformation() {
+    return this.httpClient.get<Array<Object>>(this.baseUrl + "countries");
+  }
+
+}
--- desafio-final/travel-agency/src/app/get-travel-informations/get-travel-informations.component.ts
+++ desafio-final/travel-agency/src/app/get-travel-informations/get-travel-informations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../countries-service.service';
 
 @Component({
   selector: 'get-travel-informations',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetTravelInformationsComponent implements OnInit {
 
+  countries: Array<Object> = new Array();
+
+  constructor(private countriesService: CountriesService) { }
+
   ngOnInit(): void {
+    this.countriesService.getCountriesInformation().subscribe(countries => {
+      this.countries = countries;
+      console.log(this.countries[0]);
+    })
   }
 
 }
