01e6f82f10dcd6d4337dce390f3ade78dab975ca
debora.goncalves <deboracristinapereira123@gmail.com>
2021-10-03 12:19:04 -0300

    Exibir cidades do pais selecinado - Agência de viagem

--- desafio-final/travel-agency/src/app/get-travel-informations/get-travel-informations.component.ts
+++ desafio-final/travel-agency/src/app/get-travel-informations/get-travel-informations.component.ts
@@ -10,13 +10,68 @@ import { Country } from '../country';
 export class GetTravelInformationsComponent implements OnInit {
 
   countries: Array<Country> = new Array();
+  originCountryCities: Array<any> = new Array();
+  destinyCountryCities: Array<any> = new Array();
+
+  originCountry: string = "";
+  destinyCountry: string = "";
 
   constructor(private countriesService: CountriesService) { }
 
   ngOnInit(): void {
     this.countriesService.getCountriesInformation().subscribe(countries => {
-      this.countries = countries;  
+      this.countries = countries;
     });
+  }
+
+  onChangeOriginCountry(newOriginCountry: string): void {
+    this.originCountry = newOriginCountry;
+    this.getOriginCountryCities();
+  }
+
+  onChangeDestinyCountry(newDestinyCountry: string): void {
+    this.destinyCountry = newDestinyCountry;
+    console.log(this.destinyCountry);
+    this.getDestinyCountryCities();
+  }
+
+  getOriginCountryCities(): void {
+
+    // TODO: Melhorar
+
+    for (let country of this.countries) {
+      if (country.country == "Japão" && this.originCountry == "Japão") {
+        this.originCountryCities = country.cities;
+      } else if (country.country == "Brasil" && this.originCountry == "Brasil") {
+        this.originCountryCities = country.cities;
+      } else if (country.country == "Bélgica" && this.originCountry == "Bélgica") {
+        this.originCountryCities = country.cities;
+      } else if (country.country == "Egito" && this.originCountry == "Egito") {
+        this.originCountryCities = country.cities;
+      } else if (country.country == "Canadá" && this.originCountry == "Canadá") {
+        this.originCountryCities = country.cities;
+      }
+    }
+  }
+  
+  getDestinyCountryCities(): void {
+    // TODO: Melhorar
+
+    for (let country of this.countries) {
+      if (country.country == "Japão" && this.destinyCountry == "Japão") {
+        this.destinyCountryCities = country.cities;
+        console.log(this.destinyCountryCities);
+      } else if (country.country == "Brasil" && this.destinyCountry == "Brasil") {
+        this.destinyCountryCities = country.cities;
+        console.log(this.destinyCountryCities);
+      } else if (country.country == "Bélgica" && this.destinyCountry == "Bélgica") {
+        this.destinyCountryCities = country.cities;
+      } else if (country.country == "Egito" && this.destinyCountry == "Egito") {
+        this.destinyCountryCities = country.cities;
+      } else if (country.country == "Canadá" && this.destinyCountry == "Canadá") {
+        this.destinyCountryCities = country.cities;
+      }
+    }
   }
 
 }
