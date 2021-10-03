5a94241ad3edb74dbf3f41e76099f715a2e6e34f
debora.goncalves <deboracristinapereira123@gmail.com>
2021-10-03 20:41:44 -0300

    Calcular distancia entre cidades - Agência de Viagens

--- desafio-final/travel-agency/src/app/show-travel-informations/show-travel-informations.component.ts
+++ desafio-final/travel-agency/src/app/show-travel-informations/show-travel-informations.component.ts
@@ -9,7 +9,33 @@ export class ShowTravelInformationsComponent implements OnInit {
 
   constructor() { }
 
+  // TODO: Usar outra API exceto DistanceMatrix e criar service
+
   ngOnInit(): void {
+    this.getDistanceCitiesKm();
+  }
+
+  getDistanceCitiesKm(): number {
+    let radiusEarthKm = 6371;
+
+    let latitudeOriginCity = 0;
+    let latitudeDestinyCity = 0;
+
+    let longitudeOriginCity = 0;
+    let longitudeDestinyCity = 0;
+
+    let differenceLatitude = (latitudeDestinyCity - latitudeOriginCity) * (Math.PI / 180);  // radianos
+    let differenceLongitude = (longitudeDestinyCity - longitudeOriginCity) * (Math.PI / 180);  // radianos
+
+    let a = Math.sin(differenceLatitude / 2) * Math.sin(differenceLatitude / 2) + 
+    Math.cos(latitudeOriginCity * (Math.PI / 180)) 
+    * Math.cos(latitudeDestinyCity * (Math.PI / 180)) 
+    * Math.sin(differenceLongitude / 2) * Math.sin(differenceLongitude / 2);
+   
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+
+    let distanceCitiesKM = radiusEarthKm * c;
+    return distanceCitiesKM;
   }
 
 }
